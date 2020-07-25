713c54735382a648c636ad1bdf7fbdfa75004f4c
One <5576648+One_dayday@user.noreply.gitee.com>
2020-07-25 11:05:44 +0800

    初始化

---
+++ src/main/java/com/mxh/sleeve2wxapi/Sleeve2WxApiApplication.java
@@ -0,0 +1,13 @@
+package com.mxh.sleeve2wxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sleeve2WxApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sleeve2WxApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mxh/sleeve2wxapi/Sleeve2WxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mxh.sleeve2wxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sleeve2WxApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
